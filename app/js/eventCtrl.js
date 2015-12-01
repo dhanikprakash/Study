b3113963ea9b064f84252fb5160c076c96d21849
dhanikprakash <dhanikprakash@gmail.com>
2015-12-01 20:56:47 +0000

    Angular study

---
+++ app/js/eventCtrl.js
@@ -0,0 +1,16 @@
+eventsApp.controller("eventCtrl",function($scope){
+$scope.name ="Hello World";
+    
+    $scope.data ={
+        name:"Angular Boot Camp",
+        date: "1/2/2014",
+        time:"10:30",
+        location:{
+        address:"Google  Headquarters",
+        city:"Mountain View",
+        province:"CA"
+    
+    
+    }
+    }
+});
